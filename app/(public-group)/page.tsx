730372e6eca46f396661b42a999326be7216fc84
mirnatul <mdmirnatul@gmail.com>
2026-07-22 07:14:06 +0600

    install next js shadcn and basic folder structure

---
+++ app/(public-group)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+
+      <Button size={"lg"} variant={"destructive"}>Click Me!</Button>
+    </div>
+  );
+}
